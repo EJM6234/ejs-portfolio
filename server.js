d85c3affb3766c6efc1491af126438db716cc2e0
EJ Morgan <edwardmorgan@EJs-MacBook-Pro.local>
2018-01-15 22:16:26 -0600

    Added server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("./"));
+
+app.get("/", function(req, res) {
+  res.sendFile("./index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+})
