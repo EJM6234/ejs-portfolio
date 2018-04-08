71c3463e1c31d392b8af5e4afb0c8aa94fbd7d06
EJ Morgan <edwardmorgan@EJs-MacBook-Pro.local>
2018-04-08 15:43:05 -0400

    Set up custom/local Bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,34 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
-	return gulp.src('assets/css/**/*.scss')
-		.pipe(sass())
-		.pipe(gulp.dest('assets/css'))
+	return gulp.src('assets/css/style.scss')
+		.pipe(sass({
+			includePaths: './node_modules/bootstrap-sass/assets/stylesheets'
+		}))
+		.pipe(gulp.dest('./assets/css'))
 		.pipe(browserSync.reload({
 			stream: true
-		}))
+		}));
+});
+
+gulp.task('fonts', function() {
+	return gulp.src('./node_modules/bootstrap-sass/assets/fonts/**/*')
+		.pipe(gulp.dest('./assets/fonts'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 });
 
 gulp.task('browserSync', function() {
 	browserSync.init({
 		server: {
-			baseDir: '.'
+			baseDir: './'
 		}
 	})
 });
 
 gulp.task('watch', ['browserSync', 'sass'], function() {
-	gulp.watch('assets/css/**/*.scss', ['sass']);
+	gulp.watch('assets/css/*.scss', ['sass']);
 	gulp.watch('./*.html', browserSync.reload);
 	gulp.watch('./assets/javascript/*.js', browserSync.reload);
 });
